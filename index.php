5301b92455174d9a2e8a2b487de6c5900e21b088
albertfdez96 <32301506+albertfdez96@users.noreply.github.com>
2017-11-13 20:39:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+# A-2
+
+?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <title>To do list</title>
+    <style>
+
+    body{
+
+      background-color: gray;
+
+
+    }
+
+
+  a{
+  text-decoration: none;
+  color: black;
+  text-decoration: underline;
+
+
+}
+
+#div{
+vertical-align: middle;
+text-align: right;
+  width: 50%;
+  background-color: grey;
+  position: absolute;
+
+}
+
+
+    </style>
+
+  </head>
+  <body>
+
+    <div id="div">
+      <h1>To Do List</h1>
+
+    <li><a href="signup.php">Sign Up</a></li>
+      <li><a href="signin.php">Sign In</a></li>
+
+
+    </div>
+  </body>
+</html>
